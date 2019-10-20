ee6ab08f7af776f640c2361c1e82fcc20c9306f1
Pengyixin <34302122+Pengyixin@users.noreply.github.com>
2019-10-20 20:56:40 +0800

    HexCut v1.0.1
    优化代码

--- HexCut/HexCut.cs
+++ HexCut/HexCut.cs
@@ -14,6 +14,7 @@ namespace HexCut
 {
     public partial class HexCut : Form
     {
+        const int FILE_READ_SIZE = 1024 * 1024;
         string FileName = string.Empty;
         public HexCut()
         {
@@ -23,88 +24,86 @@ namespace HexCut
         private void Button_Save_Click(object sender, EventArgs e)
         {
             long start;
-            long size;
-            byte[] sz = new byte[1024 * 1024];
+            long need_write_size;
+            long file_size;
+            byte[] tmp_byte_array = new byte[FILE_READ_SIZE];
 
             if (FileName == string.Empty)
             {
                 MessageBox.Show("请打开文件");
                 return;
             }
-  
-            FileInfo fi = new FileInfo(FileName);
 
-            //this.label1.Text = "0x" + fi.Length.ToString("X");
-            
+            BinaryReader file_read_fp = new BinaryReader(new FileStream(FileName,
+                FileMode.Open));
+            file_size = file_read_fp.BaseStream.Length;
 
-
-            if (this.TextBox_StartOffset.Text.ToString() != "")
+            if (this.TextBox_StartOffset.Text.ToString() != string.Empty)
             {
                 start = Convert.ToInt64(this.TextBox_StartOffset.Text.ToString(), 10);
-                if (start >= fi.Length - 1)
+                if (start >= file_size - 1)
                 {
-                    MessageBox.Show("请输入正确的offset,已大于文件大小");
+                    MessageBox.Show("请输入正确的offset,数值已大于文件大小");
+                    file_read_fp.Close();
                     return;
                 }
             }
             else
             {
                 MessageBox.Show("请输入offset");
+                file_read_fp.Close();
                 return;
             }
 
-            if (this.TextBox_Size.Text.ToString() != "")
+            if (this.TextBox_Size.Text.ToString() != string.Empty)
             {
-                size = Convert.ToInt64(this.TextBox_Size.Text.ToString(), 10);
-                if (size + start > fi.Length)
+                need_write_size = Convert.ToInt64(this.TextBox_Size.Text.ToString(), 10);
+                if (need_write_size + start > file_size)
                 {
                     MessageBox.Show("可写size超过文件结尾");
+                    file_read_fp.Close();
                     return;
                 }
-                if (size == 0)
-                    size = fi.Length - start;
+                if (need_write_size == 0)
+                    need_write_size = file_size - start;
             }
             else
             {
                 MessageBox.Show("请输入size,0代表一直到结尾");
+                file_read_fp.Close();
                 return;
             }
 
-            BinaryReader br = new BinaryReader(new FileStream(FileName,
-                FileMode.Open));
-            BinaryWriter bw = new BinaryWriter(new FileStream("mydata",
+
+            BinaryWriter file_write_fp = new BinaryWriter(new FileStream("mydata",
                 FileMode.Create));
 
+            file_read_fp.BaseStream.Seek(start, SeekOrigin.Begin);
 
-
-            br.BaseStream.Seek(start, SeekOrigin.Begin);
-
-            while (size >= 1024 * 1024)
+            while (need_write_size >= FILE_READ_SIZE)
             {
-                sz = br.ReadBytes(1024 * 1024);
+                tmp_byte_array = file_read_fp.ReadBytes(FILE_READ_SIZE);
 
-                bw.Write(sz, 0, 1024 * 1024);
-                size -= 1024 * 1024;
+                file_write_fp.Write(tmp_byte_array, 0, FILE_READ_SIZE);
+                need_write_size -= FILE_READ_SIZE;
             }
 
-            if (size != 0)
+            if (need_write_size != 0)
             {
-                sz = br.ReadBytes((int)size);
-                bw.Write(sz, 0, (int)size);
+                tmp_byte_array = file_read_fp.ReadBytes((int)need_write_size);
+                file_write_fp.Write(tmp_byte_array, 0, (int)need_write_size);
             }
 
-            br.Close();
-            bw.Close();
+            file_read_fp.Close();
+            file_write_fp.Close();
         }
 
         private void OpenToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             if (this.Openfile.ShowDialog() == DialogResult.OK)
-                //选择文件后，用openFileDialog1的FileName属性获取文件的绝对路径
                 FileName = this.Openfile.FileName;
             if (FileName == string.Empty)
                 return;
-            //this.label1.Text = "0x" + fi.Length.ToString("X");
             FileInfo fi = new FileInfo(FileName);
             this.Lable_FileNameText.Text = System.IO.Path.GetFileName(FileName);
             this.Lable_FileSizeText.Text = fi.Length.ToString();
